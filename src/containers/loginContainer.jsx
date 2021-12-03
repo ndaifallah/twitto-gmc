ce2e8943c94fb20573c2528d2b9a71e69e72df0f
SpaceCowboy20 <housseine18@gmail.com>
2021-12-03 12:35:38 +0100

    test

---
+++ src/containers/loginContainer.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class LoginContainer extends Component {
+    render() {
+        return (
+            <div style={{
+                width:'500px',
+                height:'500',
+                background:' rgba(255, 255, 255, 0.2)',
+                borderRadius:' 16px',
+                boxShadow:' 0 4px 30px rgba(0, 0, 0, 0.1)',
+                backdropFilter: 'blur(5px)',
+                webkitBackdropFilter:' blur(5px)',
+                border:' 1px solid rgba(255, 255, 255, 0.3)',
+            }}>
+                
+            </div>
+        )
+    }
+}
+
+
+export default LoginContainer
