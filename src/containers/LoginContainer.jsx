acffed731133e2cf65789703de4b8cce801373d2
Ghiles55 <ghilesmekdem@gmail.com>
2021-12-04 11:05:38 +0100

    Updated Login and Login and LoginContainer.

---
+++ src/containers/LoginContainer.jsx
@@ -0,0 +1,30 @@
+
+
+
+import React, { Component } from 'react';
+import Login from '../components/login';
+
+class LoginContainer extends Component {
+    render() {
+        return (
+            <div style={{
+                width:'350px',
+                height:'350px',
+                display:'flex',
+                justifyContent:'center',
+                alignItems:'center',
+                background:' rgba(255, 255, 255, 0.2)',
+                borderRadius:' 16px',
+                boxShadow:' 0 4px 30px rgba(0, 0, 0, 0.1)',
+                backdropFilter: 'blur(5px)',
+                webkitBackdropFilter:' blur(5px)',
+                border:' 1px solid rgba(255, 255, 255, 0.3)',
+            }}>
+                <Login></Login>
+            </div>
+        )
+    }
+}
+
+
+export default LoginContainer
